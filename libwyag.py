3e936a92a5eeb171daa8e86eef180408508ef577
Dipam Vasani <dipam44@gmail.com>
2020-04-25 01:10:06 -0400

    Write GitRepo wrapper and some utility functions

--- libwyag.py
+++ libwyag.py
@@ -34,3 +34,84 @@ def main(args = sys.argv[1:]):
     elif args.command == "rm"          : cmd_rm(args)
     elif args.command == "show-ref"    : cmd_show_ref(args)
     elif args.command == "tag"         : cmd_tag(args)
+
+# abstraction for a git repository
+class GitRepository(object):
+    """A git repository"""
+    # a git repo contains 2 things, worktree which is the folder we want to apply version control on
+    # and a .git repo where git stores its own things
+    # the config file is stored in .git/config
+
+    worktree = None
+    gitdir = None
+    conf = None
+    
+    # an additional force parameter to disable checks
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+        
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a git repository %s" % path)
+        
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+        
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported repositoryformatversion %s " %vers)
+
+
+
+# we will be doing a lot of path manipulations hence we will write some utility functions
+def repo_path(repo, *path):
+    """Compute path under repo's gitdir"""
+    return os.path.join(repo.gitdir, *path)
+
+
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path, but creates dirname(*path) if absent. For example repo_file(r, "refs", "remotes", "origin")
+    will create .git/refs/remotes."""
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir"""
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
